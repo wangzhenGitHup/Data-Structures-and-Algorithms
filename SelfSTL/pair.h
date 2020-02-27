c4417e5ff59e31e884d2e08fc7c28118959daaec
wangzhen <463208406@qq.com>
2020-02-27 22:34:34 +0800

    实现pair

---
+++ SelfSTL/pair.h
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace SelfSTL
+{
+	template<class T1, class T2>
+	struct pair
+	{
+		typedef T1 first_type;
+		typedef T2 second_type;
+
+		T1 first;
+		T2 second;
+
+		pair() : first(T1()), second(T2()) {}
+
+		template<class U, class V>
+		pair(const pair<U, V>& p) : first(p.first), second(p.second) {}
+
+		bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{
+			return x.first == y.first && x.second == y.second;
+		}
+
+		bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{
+			return x.first < y.first ||
+				(!(y.first < x.first) && x.second < y.second);
+		}
+	};
+
+	template<class T1, class T2>
+	pair<T1, T2> make_pair(const T1& x, const T2& y)
+	{
+		return pair<T1, T2>(x, y);
+	}
+}
