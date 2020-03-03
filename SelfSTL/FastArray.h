7c3d88a6d0af6d6aa6266dc11ecaac523a0a801a
wangzhen <463208406@qq.com>
2020-03-03 14:23:00 +0800

    包装数组，《The C++ Programming Language>3rd 书中例子

--- SelfSTL/FastArray.h
+++ SelfSTL/FastArray.h
@@ -1,31 +1,33 @@
 #pragma once
 
 #include <cstddef>
-
-template<class T, std::size_t arraySize>
-class FastArray
+namespace SelfSTL
 {
-public:
-	typedef T               value_type;
-	typedef T*              iterator;
-	typedef const T*        const_iterator;
-	typedef T&              reference;
-	typedef const T&        const_reference;
-	typedef std::size_t     size_type;
-	typedef std::ptrdiff_t  difference_type;
+	template<class T, std::size_t arraySize>
+	class FastArray
+	{
+	public:
+		typedef T               value_type;
+		typedef T*              iterator;
+		typedef const T*        const_iterator;
+		typedef T&              reference;
+		typedef const T&        const_reference;
+		typedef std::size_t     size_type;
+		typedef std::ptrdiff_t  difference_type;
 
-	iterator begin() { return data; }
-	const_iterator begin()const { return data; }
-	iterator end() { return data + arraySize; }
-	const_iterator end() const { return data + arraySize; }
-	reference operator[](std::size_t idx){ return data[idx]; }
-	const_reference operator[](std::size_t idx) const { return data[idx]; }
+		iterator begin() { return data; }
+		const_iterator begin()const { return data; }
+		iterator end() { return data + arraySize; }
+		const_iterator end() const { return data + arraySize; }
+		reference operator[](std::size_t idx){ return data[idx]; }
+		const_reference operator[](std::size_t idx) const { return data[idx]; }
 
-	size_type size() const { return arraySize; }
-	size_type max_size() const{ return arraySize; }
+		size_type size() const { return arraySize; }
+		size_type max_size() const{ return arraySize; }
 
-	T* as_array() { return data; }
+		T* as_array() { return data; }
 
-private:
-	T data[arraySize];
-};
+	private:
+		T data[arraySize];
+	};
+}
