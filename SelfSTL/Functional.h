700a3589e7ca6524e0274158c01c783307550742
wangzhen <463208406@qq.com>
2020-04-07 16:58:25 +0800

    添加Functional实现文件

---
+++ SelfSTL/Functional.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace SelfSTL
+{
+	template<class T>
+	struct less
+	{
+		typedef T first_argument_type;
+		typedef T second_argument_type;
+		typedef bool result_type;
+
+		result_type operator()(const first_argument_type& x, const second_argument_type& y)
+		{
+			return x < y;
+		}
+	};
+
+	template<class T>
+	struct equal_to
+	{
+		typedef T first_argument_type;
+		typedef T second_argument_type;
+		typedef bool result_type;
+
+		result_type operator()(const first_argument_type& x, const second_argument_type& y)
+		{
+			return x == y;
+		}
+	};
+}
